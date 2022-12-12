c56c4ae1bae2ec0ea2d5e4e6c5224db4ba452365
Tsaqif06 <tsaqifcakep87@gmail.com>
2022-12-12 16:17:41 +0700

    menggunakan javascript dasar

---
+++ script.js
@@ -0,0 +1,43 @@
+//Declaration
+let hasil = '';
+let lagi = true;
+
+//Looping
+while (lagi == true) {
+//User Memilih
+let user = prompt('Game Batu Gunting Kertas\nSilahkan Pilih : \n Batu\n Gunting\n Kertas');
+
+//Lawan Memilih
+let enemy = Math.floor(Math.random() * 3);
+let enemy2 = 'Batu';
+if (enemy == 1 || enemy2 == 1) {
+   enemy = 'Batu'
+   enemy2 = 'batu'
+} else if (enemy == 2 || enemy2 == 2) {
+   enemy = 'Gunting'
+   enemy2 = 'gunting'
+} else {
+   enemy = 'Kertas'
+   enemy2 = 'kertas'
+}
+
+//Rules
+if (user == enemy || user == enemy2) {
+   hasil = 'SERI';
+} else if (user == 'Batu' || user == 'batu') {
+   hasil = (enemy == 'Gunting' && enemy2 == 'gunting') ? 'KAMU MENANG!' : 'KAMU KALAH!';
+} else if (user == 'Gunting' || user == 'gunting') {
+   hasil = (enemy == 'Kertas' && enemy2 == 'kertas') ? 'KAMU MENANG!' : 'KAMU KALAH!';
+} else if (user == 'Kertas' || user == 'kertas') {
+   hasil = (enemy == 'Batu' && enemy2 == 'batu') ? 'KAMU MENANG!' : 'KAMU KALAH!';
+} else {
+   hasil = 'Pilihan Salah!'
+} 
+   
+
+//Display Output
+alert('Kamu Memilih : '+ user +'\nLawan Memilih : '+ enemy +'\n'+ hasil);
+
+lagi = confirm('Lagi?')
+}
+alert('Terima Kasih Sudah Bermain!')
